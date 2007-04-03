de976f155296f735ceb49446013ae93d38c2d5d3
dkavanagh <dkavanagh@7ed6c317-272d-0410-8ab0-49a3fbf2961c>
2007-04-03 15:06:46 +0000

    tagging version 0.2

---
+++ trunk/java/com/xerox/amazonws/sqs/MessageQueue.java
@@ -0,0 +1,297 @@
+package com.xerox.amazonws.sqs;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
+import com.xerox.amazonws.common.JAXBuddy;
+import com.xerox.amazonws.sqs.jaxb.DeleteMessageResponse;
+import com.xerox.amazonws.sqs.jaxb.DeleteQueueResponse;
+import com.xerox.amazonws.sqs.jaxb.PeekMessageResponse;
+import com.xerox.amazonws.sqs.jaxb.ReceiveMessageResponse;
+import com.xerox.amazonws.sqs.jaxb.SendMessageResponse;
+
+/**
+ * This class provides an interface with the Amazon SQS message queue. It provides methods
+ * for sending / receiving messages and deleting queues and messsages on queues.
+ *
+ * @author D. Kavanagh
+ * @author developer@dotech.com
+ */
+public class MessageQueue extends QueueService {
+    public static final int MAX_MESSAGES = 600;
+    public static final int MAX_MESSAGE_BODIES_SIZE = 4096;
+
+	protected String queueId;
+
+    protected MessageQueue(String queueUrl, String awsAccessKeyId,
+							String awsSecretAccessKey, boolean isSecure,
+							String server) throws SQSException {
+        super(awsAccessKeyId, awsSecretAccessKey, isSecure, server);
+		queueId = queueUrl.substring(queueUrl.indexOf("//")+2);
+		queueId = queueId.substring(queueId.indexOf("/")+1);
+    }
+
+	/**
+	 * This method provides the URL for the message queue represented by this object.
+	 *
+	 * @return generated queue service url
+	 */
+	public URL getUrl() {
+		try {
+			return new URL(super.getUrl().toString()+queueId);
+		} catch (MalformedURLException ex) {
+			return null;
+		}
+	}
+
+	/**
+	 * Sends a message to a specified queue. The message must be between 1 and 256K bytes long.
+	 *
+	 * @param msg the message to be sent (should be base64 encoded)
+	 * @return the message id for the message just sent
+	 */
+    public String sendMessage(String msg) throws SQSException {
+		try {
+			String request = queueId+"/back";
+			URLConnection conn = makeRequest("PUT", request, super.headers);
+			conn.setDoOutput(true);
+			OutputStream oStr = conn.getOutputStream();
+			oStr.write(new String(msg).getBytes());
+			oStr.flush();
+			InputStream iStr = conn.getInputStream();
+			SendMessageResponse response = JAXBuddy.deserializeXMLStream(SendMessageResponse.class, iStr);
+			return response.getMessageId();
+		} catch (JAXBException ex) {
+			throw new SQSException("Problem parsing returned message.", ex);
+		} catch (MalformedURLException ex) {
+			throw new SQSException(ex.getMessage(), ex);
+		} catch (IOException ex) {
+			throw new SQSException(ex.getMessage(), ex);
+		}
+	}
+
+	/**
+	 * Attempts to receive a message from the queue. The queue default visibility timeout
+	 * is used.
+	 *
+	 * @return the message object
+	 */
+    public Message receiveMessage() throws SQSException {
+        Message amessage[] = receiveMessages(BigInteger.valueOf(1L), ((BigInteger) (null)));
+        if(amessage.length > 0)
+            return amessage[0];
+        else
+            return null;
+	}
+
+	/**
+	 * Attempts to receive a message from the queue.
+	 *
+	 * @param visibilityTimeout the duration (in seconds) the retrieved message is hidden from
+	 *                          subsequent calls to retrieve.
+	 * @return the message object
+	 */
+    public Message receiveMessage(int visibilityTimeout) throws SQSException {
+        Message amessage[] = receiveMessages(BigInteger.valueOf(1L), BigInteger.valueOf(visibilityTimeout));
+        if(amessage.length > 0)
+            return amessage[0];
+        else
+            return null;
+	}
+
+	/**
+	 * Attempts to retrieve a number of messages from the queue. If less than that are availble,
+	 * the max returned is the number of messages in the queue, but not necessarily all messages
+	 * in the queue will be returned. The queue default visibility timeout is used.
+	 *
+	 * @param numMessages the maximum number of messages to return
+	 * @return an array of message objects
+	 */
+    public Message[] receiveMessages(int numMessages) throws SQSException {
+        return receiveMessages(BigInteger.valueOf(numMessages), ((BigInteger) (null)));
+	}
+
+	/**
+	 * Attempts to retrieve a number of messages from the queue. If less than that are availble,
+	 * the max returned is the number of messages in the queue, but not necessarily all messages
+	 * in the queue will be returned.
+	 *
+	 * @param numMessages the maximum number of messages to return
+	 * @param visibilityTimeout the duration (in seconds) the retrieved message is hidden from
+	 *                          subsequent calls to retrieve.
+	 * @return an array of message objects
+	 */
+    public Message[] receiveMessages(int numMessages, int visibilityTimeout) throws SQSException {
+        return receiveMessages(BigInteger.valueOf(numMessages), BigInteger.valueOf(visibilityTimeout));
+	}
+
+	/**
+	 * Internal implementation of receiveMessages.
+	 *
+	 * @param
+	 * @return
+	 */
+    protected Message[] receiveMessages(BigInteger numMessages, BigInteger visibilityTimeout) throws SQSException {
+		try {
+			String request = queueId+"/front?NumberOfMessages="+numMessages;
+			if (visibilityTimeout != null) request += "&VisibilityTimeout="+visibilityTimeout;
+			HttpURLConnection conn = makeRequest("GET", request, super.headers);
+			if (conn.getResponseCode() < 400) {
+				InputStream iStr = conn.getInputStream();
+				ReceiveMessageResponse response = JAXBuddy.deserializeXMLStream(ReceiveMessageResponse.class, iStr);
+				if (response.getMessage() == null) {
+					return new Message[0];
+				}
+				else {
+					ArrayList<Message> msgs = new ArrayList();
+					for (com.xerox.amazonws.sqs.jaxb.Message msg : response.getMessage()) {
+						msgs.add(new Message(msg.getMessageId(), msg.getMessageBody()));
+					}
+					return msgs.toArray(new Message [msgs.size()]);
+				}
+			}
+			else {
+				return new Message[0];
+			}
+		} catch (JAXBException ex) {
+			throw new SQSException("Problem parsing returned message.", ex);
+		} catch (MalformedURLException ex) {
+			throw new SQSException(ex.getMessage(), ex);
+		} catch (IOException ex) {
+			throw new SQSException(ex.getMessage(), ex);
+		}
+	}
+
+	/**
+	 * Returns a specified message. This does not affect and is not affected by the visibility
+	 * timeout of either the queue or the message.
+	 *
+	 * @param
+	 * @return
+	 */
+    public Message peekMessage(String msgId) throws SQSException {
+		try {
+			InputStream iStr = makeRequest("GET", queueId+"/"+msgId, super.headers).getInputStream();
+			PeekMessageResponse response = JAXBuddy.deserializeXMLStream(PeekMessageResponse.class, iStr);
+			com.xerox.amazonws.sqs.jaxb.Message msg = response.getMessage();
+			if (msg == null) {
+				return null;
+			}
+			else {
+				return new Message(msg.getMessageId(), msg.getMessageBody());
+			}
+		} catch (JAXBException ex) {
+			throw new SQSException("Problem parsing returned message.", ex);
+		} catch (MalformedURLException ex) {
+			throw new SQSException(ex.getMessage(), ex);
+		} catch (IOException ex) {
+			throw new SQSException(ex.getMessage(), ex);
+		}
+	}
+
+	/**
+	 * Deletes the message identified by msgid on the queue this object represents.
+	 *
+	 * @param msgId the id of the message to be deleted
+	 */
+    public void deleteMessage(String msgId) throws SQSException {
+		try {
+			HttpURLConnection conn = makeRequest("DELETE", queueId+"/"+msgId, super.headers);
+			if (conn.getResponseCode() < 400) {
+				InputStream iStr = conn.getInputStream();
+				DeleteMessageResponse response = JAXBuddy.deserializeXMLStream(DeleteMessageResponse.class, iStr);
+			}
+			else {
+				throw new SQSException("Error deleting message id="+msgId);
+			}
+		} catch (JAXBException ex) {
+			throw new SQSException("Problem parsing returned message.", ex);
+		} catch (MalformedURLException ex) {
+			throw new SQSException(ex.getMessage(), ex);
+		} catch (IOException ex) {
+			throw new SQSException(ex.getMessage(), ex);
+		}
+	}
+
+	/**
+	 * Deletes the message queue represented by this object.
+	 */
+    public void deleteQueue() throws SQSException {
+        int respCode;
+		try {
+			HttpURLConnection conn = makeRequest("DELETE", queueId, super.headers);
+			if ((respCode = conn.getResponseCode()) < 400) {
+				InputStream iStr = conn.getInputStream();
+				DeleteQueueResponse response = JAXBuddy.deserializeXMLStream(DeleteQueueResponse.class, iStr);
+			}
+			else {
+				throw new SQSException("Error deleting queue, response code = "+respCode);
+			}
+		} catch (JAXBException ex) {
+			throw new SQSException("Problem parsing returned message.", ex);
+		} catch (MalformedURLException ex) {
+			throw new SQSException(ex.getMessage(), ex);
+		} catch (IOException ex) {
+			throw new SQSException(ex.getMessage(), ex);
+		}
+	}
+
+	/**
+	 * Placeholder. Not implemented.
+	 */
+    public int getVisibilityTimeout() throws SQSException {
+		return 0;
+	}
+
+	/**
+	 * Placeholder. Not implemented.
+	 */
+    public void setVisibilityTimeout(int timeout) throws SQSException {
+	}
+
+	/**
+	 * Placeholder. Not implemented.
+	 */
+    public void setVisibilityTimeout(String msgId, int timeout) throws SQSException {
+	}
+
+	/**
+	 * Placeholder. Not implemented.
+	 */
+    public void setVisibilityTimeout(String[] msgIds, int timeout) throws SQSException {
+	}
+
+/*  grants not supported in REST at this time
+    public void addGrantByEmail(String, String) throws Exception {
+	}
+
+    public void addGrantByCustomerId(String, String, String) throws Exception {
+	}
+
+    public void removeGrantByEmailAddress(String, String) throws Exception {
+	}
+
+    public void removeGrantByCustomerId(String, String) throws Exception {
+	}
+    public Grant[] listGrants(Grantee, String) throws Exception {
+	}
+*/
+
+	public static List<MessageQueue> createList(String [] queueUrls, String awsAccessKeyId, String awsSecretAccessKey, boolean isSecure, String server) throws SQSException {
+		ArrayList<MessageQueue> ret = new ArrayList<MessageQueue>();
+		for (int i=0; i<queueUrls.length; i++) {
+			ret.add(new MessageQueue(queueUrls[i], awsAccessKeyId, awsSecretAccessKey, isSecure, server));
+		}
+		return ret;
+	}
+}
---
+++ trunk/java/com/xerox/amazonws/sqs/SQSUtils.java
@@ -0,0 +1,105 @@
+package com.xerox.amazonws.sqs;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This class provides helper methods to interact with the Amazon Simple Queue Service.
+ *
+ * @author D. Kavanagh
+ * @author developer@dotech.com
+ */
+public class SQSUtils {
+    private static final Logger logger = Logger.getLogger(SQSUtils.class.getName());
+    
+	/**
+	 * Returns a message queue for a given name, credentials. Uses the default SQS host.
+	 *
+	 * @param queueName name of the queue to connect to
+	 * @param accessKey AWS access id
+	 * @param secretKey AWS secret key
+	 * @return object representing the message queue
+	 */
+	public static MessageQueue connectToQueue(String queueName, String accessKey, String secretKey) 
+			throws SQSException {
+		return connectToQueue(null, queueName, accessKey, secretKey);
+	}
+
+	/**
+	 * Returns a message queue for a given name, credentials.
+	 *
+	 * @param serverName name of the host to use
+	 * @param queueName name of the queue to connect to
+	 * @param accessKey AWS access id
+	 * @param secretKey AWS secret key
+	 * @return object representing the message queue
+	 */
+	public static MessageQueue connectToQueue(String serverName, String queueName,
+                                              String accessKey, String secretKey) 
+			throws SQSException {
+		// Create the service object
+		QueueService service = getQueueService(accessKey, secretKey, serverName);
+		 
+		// Retrieve the message queue object (by name).
+		return getMessageQueue(service, queueName);
+	}
+
+	/**
+	 * Create a QueueService object for a given URL.
+	 *
+	 * @param accessKey AWS access id
+	 * @param secretKey AWS secret key
+	 * @param serverName name of the host to use
+	 * @return object representing the queue service
+	 */
+	public static QueueService getQueueService(String accessKey, String secretKey, String serverName) 
+			throws SQSException {
+		QueueService service = null;
+		if (serverName != null) {
+			service = new QueueService(accessKey, secretKey, true, serverName);
+		}
+		else {
+			service = new QueueService(accessKey, secretKey);
+		}
+		if (service.getUrl() != null) {
+			logger.info( "Service: " + service.getUrl().toString() );
+		} else {
+			logger.error( "Service: null url!" );
+		}
+		return service;
+	}
+	
+	
+	/**
+	 * Looks for a queue by name: if found, return a MessageQuueue object for it.
+	 * Else, return null.
+	 *
+	 * @param service the queue service we're using
+	 * @param msgQueueName the name of the message queue to find, or create
+	 * @return object representing the message queue
+	 */
+	public static MessageQueue getMessageQueue(QueueService service, String msgQueueName)
+			throws SQSException {
+		MessageQueue msgQueue = null;
+		MessageQueue msgQueueFound = null;
+		List<MessageQueue> msgQueuesFound = service.listMessageQueues( null );
+		for ( MessageQueue mq : msgQueuesFound ) {
+			if ( mq.getUrl().toString().endsWith( msgQueueName ) ) {
+				msgQueueFound = mq;
+			}
+		}
+		if (msgQueueFound == null) {
+			logger.info("Message queue couldn't be listed, going to create it.");
+			msgQueue = service.getOrCreateMessageQueue(msgQueueName.substring(msgQueueName.lastIndexOf("/")+1));
+		} else if (msgQueue == null) {
+			msgQueue = msgQueueFound;
+		}
+		if (msgQueue == null) {
+			logger.error( "Couldn't find message queue " + msgQueueName);
+		} else {
+			logger.info( "Using message queue resource at " + msgQueue.getUrl() ); 
+		}
+		return msgQueue;
+	}
+}
