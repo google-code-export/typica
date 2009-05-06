c66f71bff947b957d2771158f844e07e36edc63f
dkavanagh <dkavanagh@7ed6c317-272d-0410-8ab0-49a3fbf2961c>
2009-05-06 17:51:00 +0000

    tagging the 1.5.2 release

---
+++ tags/v1.5.2/java/com/xerox/amazonws/sqs2/QueueAttribute.java
@@ -0,0 +1,38 @@
+//
+// typica - A client library for Amazon Web Services
+// Copyright (C) 2007 Xerox Corporation
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package com.xerox.amazonws.sqs2;
+
+public enum QueueAttribute {
+	ALL ("All"),
+	APPROXIMATE_NUMBER_OF_MESSAGES ("ApproximateNumberOfMessages"),
+	CREATED_TIMESTAMP ("CreatedTimestamp"),
+	LAST_MODIFIED_TIMESTAMP ("LastModifiedTimestamp"),
+	VISIBILITY_TIMEOUT ("VisibilityTimeout"),
+	REQUEST_PAYER ("RequestPayer"),
+	POLICY ("POLICY");
+
+	private final String queryAttribute;
+
+	QueueAttribute(String queryAttr) {
+		queryAttribute = queryAttr;
+	}
+
+	String queryAttribute() {
+		return queryAttribute;
+	}
+}
